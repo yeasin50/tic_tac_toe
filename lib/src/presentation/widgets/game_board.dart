e0a5cdb38ce5e25e6a7bc28a31fc6b1893eadb8b
Md. Yeasin Sheikh <yeasinsheikh50@gmail.com>
2024-07-15 10:32:02 +0600

    added-isolate

--- lib/src/presentation/widgets/game_board.dart
+++ lib/src/presentation/widgets/game_board.dart
@@ -64,12 +64,13 @@ class _GameBoardState extends State<TickTacToeGameBoard> {
     super.dispose();
   }
 
-  void aiTap() {
-    final int? index = widget.oPlayer!.findBestMove(engine.data);
+  void aiTap() async {
+    final int? index = await widget.oPlayer!.findBestMove(engine.data);
 
     if (index != null) {
       engine.onOPressed(index: index);
       isXPlayer = true;
+      updateGameState();
     }
   }
 
